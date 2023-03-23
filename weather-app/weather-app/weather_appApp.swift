3246f478b0788cc43af544598ae75dd4ce69c2d3
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-03-22 22:36:14 -0700

    defined the UI using SwiftUI

---
+++ weather-app/weather-app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather-app
+//
+//  Created by Jonathan Bigbee on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
