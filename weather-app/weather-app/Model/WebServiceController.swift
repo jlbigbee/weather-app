e8834caa528657665e24465b72a12f7253dad5a6
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-03-22 22:48:35 -0700

    implemented WebServiceController protocol

---
+++ weather-app/weather-app/Model/WebServiceController.swift
@@ -0,0 +1,18 @@
+//
+//  WebServiceController.swift
+//  weather-app
+//
+//  Created by Jonathan Bigbee on 3/22/23.
+//
+
+import Foundation
+
+public enum WebServiceControllerError: Error {
+    case invalidURL(String)
+    case invalidPayload(URL)
+    case forwarded(Error)
+}
+
+public protocol WebServiceController {
+    func fetchWeatherData(for city: String, completionHandler: (String?, WebServiceControllerError?) -> Void)
+}
