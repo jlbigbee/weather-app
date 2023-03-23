f89b3a01ce79ed1de9a286c0ef5c7f0a00b7490e
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-03-23 00:54:36 -0700

    completed the OpenWeatherMap controller

--- weather-app/weather-app/Model/OpenWeatherMapController.swift
+++ weather-app/weather-app/Model/OpenWeatherMapController.swift
@@ -13,16 +13,45 @@ private enum API {
 
 class OpenWeatherMapController: WebServiceController {
     func fetchWeatherData(for city: String, completionHandler: (String?, WebServiceControllerError?) -> Void) {
-//    api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+        //    api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
         let endpoint = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(API.key)"
         
         guard let safeURLString = endpoint.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed),
-            let endpointURL = URL(string: safeURLString) else {
+              let endpointURL = URL(string: safeURLString) else {
             completionHandler(nil, WebServiceControllerError.invalidURL(endpoint))
             return
         }
+        
+        let dataTask = URLSession.shared.dataTask(with: endpointURL) { (data, response, error) in
+            guard error == nil else {
+                completionHandler(nil, WebServiceControllerError.forwarded(error!))
+                return
+            }
+            guard let responseData = data else {
+                completionHandler(nil, WebServiceControllerError.invalidPayload(endpointURL))
+                return
+            }
+            
+            
+            // decode json
+            let decoder = JSONDecoder()
+            do {
+                let weatherList = try decoder.decode(OpenWeatherMapContainer.self, from: responseData)
+                guard let weatherInfo = weatherList.list?.first,
+                      let weather = weatherInfo.weather.first?.main,
+                      let temperature = weatherInfo.main.temp else {
+                    completionHandler(nil, WebServiceControllerError.invalidPayload(endpointURL))
+                    return
+                }
+                
+                // compose weather information
+                let weatherDescription = "\(weather) \(temperature) °F"
+                completionHandler(weatherDescription, nil)
+            } catch let error {
+                completionHandler(nil, WebServiceControllerError.forwarded(error))
+            }
+        }
+        dataTask.resume()
     }
-    
-    
 }
 
